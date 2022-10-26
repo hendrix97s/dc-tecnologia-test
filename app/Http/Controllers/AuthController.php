00913239ea26bafe030f121fc22c57ae7bf4f47b
Luiz F. Lima <lf.system@outlook.com>
2022-10-26 20:04:09 -0300

    Configurado controller de login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,18 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\AuthenticateRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     //
-
-    public function login(Request $request)
+    public function authenticate(AuthenticateRequest $request)
     {
-
+      $credentials = $request->validated();
+      $user = User::where('email', $credentials['email'])->first();
+      if(!$user)
+        return back()->withErrors(['email' => 'Email not found.']);
+      
+      if(!auth()->attempt($credentials))
+        return back()->withErrors(['password' => 'Password is incorrect.']);
+  
+      $user->save();
+      redirect()->route('dashboard');      
     }
 
     public function logout()
     {
-
+      Auth::logout();
+      return redirect()->route('login');
     }
 }
