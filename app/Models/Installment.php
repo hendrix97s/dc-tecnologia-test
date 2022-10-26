26eed099fe7588ed81bb6fab25d61e02b83b7db2
Luiz F. Lima <lf.system@outlook.com>
2022-10-26 18:50:38 -0300

    Feito diagrama do test e criado base de migrations, models, controller etc

---
+++ app/Models/Installment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Installment extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+      'sale_id',
+      'amount',
+      'due_date',
+      'paid',
+    ];
+}
---
+++ database/migrations/2022_10_26_212540_create_products_has_sales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_has_sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('sale_id')->constrained();
+            $table->unsignedInteger('quantity');
+            $table->decimal('total', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products_has_sales');
+    }
+};
